e2735c319c781fd16d3b9d3db577be9538bf1413
David Hurley <dhurls99@gmail.com>
2023-03-01 20:04:25 -0500

    try this

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,7 +2,22 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.17',
+  solidity: {
+    version: '0.8.17',
+  },
+  networks: {
+    // for testnet
+    'base-goerli': {
+      url: 'https://goerli.base.org',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+    // for local dev environment
+    'base-local': {
+      url: 'http://localhost:8545',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+  },
+  defaultNetwork: 'hardhat',
 };
 
 export default config;
