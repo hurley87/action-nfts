8ba13dcb5c598c0273963f500c570c5824cd3704
David Hurley <dhurls99@gmail.com>
2023-03-01 20:07:03 -0500

    ignore hardhat

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,23 +1,23 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
+// import { HardhatUserConfig } from 'hardhat/config';
+// import '@nomicfoundation/hardhat-toolbox';
 
-const config: HardhatUserConfig = {
-  solidity: {
-    version: '0.8.17',
-  },
-  networks: {
-    // for testnet
-    'base-goerli': {
-      url: 'https://goerli.base.org',
-      accounts: [process.env.WALLET_KEY as string],
-    },
-    // for local dev environment
-    'base-local': {
-      url: 'http://localhost:8545',
-      accounts: [process.env.WALLET_KEY as string],
-    },
-  },
-  defaultNetwork: 'hardhat',
-};
+// const config: HardhatUserConfig = {
+//   solidity: {
+//     version: '0.8.17',
+//   },
+//   networks: {
+//     // for testnet
+//     'base-goerli': {
+//       url: 'https://goerli.base.org',
+//       accounts: [process.env.WALLET_KEY as string],
+//     },
+//     // for local dev environment
+//     'base-local': {
+//       url: 'http://localhost:8545',
+//       accounts: [process.env.WALLET_KEY as string],
+//     },
+//   },
+//   defaultNetwork: 'hardhat',
+// };
 
-export default config;
+// export default config;
