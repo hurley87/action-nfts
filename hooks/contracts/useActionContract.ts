aa290df7fb9a032446df6bf4434da01df6e2892d
David Hurley <dhurls99@gmail.com>
2023-03-05 06:09:38 -0500

    add latest

---
+++ hooks/contracts/useActionContract.ts
@@ -0,0 +1,30 @@
+import { useContract, useProvider, useSigner } from 'wagmi';
+import { ethers } from 'ethers';
+
+import ActionContract from './abis/Action.json';
+
+const useActionContract = () => {
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+  const contract = useContract({
+    address: '0x7eb20Fe67Fca0cC12736A69d918968b6913617DB', // Add the address that was output from your deploy script, e.g. '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'.
+    abi: ActionContract.abi,
+    signerOrProvider: signer || provider,
+  });
+
+  const mint = async (to: string, tokenId: string) => {
+    const tx = await contract?.mint(to, tokenId, {
+      gasLimit: ethers.utils.hexlify(1000000),
+      value: ethers.utils.parseEther('0.025'),
+    });
+    const receipt = await tx?.wait();
+    return receipt;
+  };
+
+  return {
+    contract,
+    mint,
+  };
+};
+
+export default useActionContract;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,63 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { goerli } from 'wagmi/chains';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { QueryClient, QueryClientProvider, QueryCache } from 'react-query';
+
+const { chains, provider } = configureChains(
+  [goerli],
+  [
+    jsonRpcProvider({
+      priority: 0,
+      rpc: () => ({
+        http: 'https://thrumming-yolo-liquid.ethereum-goerli.quiknode.pro/2914ca7542a25121e666335be39cf66cd5f882e6/',
+      }),
+    }),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+// Create a react-query client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: () => {
+      // toast.error(
+      //   "Network Error: Ensure Metamask is connected & on the same network that your contract is deployed to."
+      // );
+    },
+  }),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ChakraProvider>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </ChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
