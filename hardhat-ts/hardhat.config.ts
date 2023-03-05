1f999390a39e948703335cc2e0c60f54acf249a5
David Hurley <dhurls99@gmail.com>
2023-03-05 07:00:19 -0500

    add config

---
+++ hardhat-ts/hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+require('dotenv').config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.17',
+  },
+  networks: {
+    // for testnet
+    goerli: {
+      url: 'https://goerli.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+    // for local dev environment
+    'base-local': {
+      url: 'http://localhost:8545',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+  },
+  defaultNetwork: 'hardhat',
+};
+
+export default config;
